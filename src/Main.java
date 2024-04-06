1612ab85a188e5fdac319c20b0e347251b1c8efb
Aidos Zhasulan <aidoszhasulan155@gmail.com>
2024-04-07 00:30:52 +0300

    GetMean

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,26 @@
 
 public class Main {
     /*
-    * This method calculates the lowest number in array
-    * it uses recursive approach
-    * Time complexity: O(n) ,where n is the length of array
-    * The recursion runs within array from length-1 to 0
-    * resulting in linear time complexity
     *
-    * @param length is length of input array, m is input array , lowest is number in array which we must compare in array
-    *@return the lowest number in array
     * */
-    public static int lowestNumber(int legnth,int[] m,int lowest) {
-        if(legnth==0)
-        {
-            return lowest;
+    public static double getMean(int currIndex, int[] m,double sum) {
+        if (currIndex == m.length) {
+            return (double) sum / m.length;
         }
-        if(m[legnth]<lowest)
-        {
-            lowest=m[legnth];
-        }
-        return lowestNumber(legnth-1,m,lowest);
+        return getMean(currIndex + 1,m,m[currIndex]+sum);
     }
-    public static int lowestNumber()
+
+    public static double getMean()
     {
         int[] m={3,4,1,5,-12};
-        return lowestNumber(m.length-1,m,0);
+        return getMean(0,m,0);
     }
     public static void main(String[] args) {
         double startTime=System.nanoTime();
-        int result =lowestNumber();
+        double result = getMean();
         double endTime =System.nanoTime();
         double duration =(endTime-startTime)/1000000;
-        System.out.println("Lowest number "+result);
+        System.out.println("mean number "+result);
         System.out.println("Duration "+duration);
     }
 }
