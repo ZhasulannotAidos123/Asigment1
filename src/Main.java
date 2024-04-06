99bb80073516f8838ea289e7794e7e210354e8b5
Aidos Zhasulan <aidoszhasulan155@gmail.com>
2024-04-07 00:07:07 +0300

    LowestNumber

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+
 public class Main {
+    public static int lowestNumber(int legnth,int[] m,int lowest) {
+        if(legnth==0)
+        {
+            return lowest;
+        }
+        if(m[legnth]<lowest)
+        {
+            lowest=m[legnth];
+        }
+        return lowestNumber(legnth-1,m,lowest);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
